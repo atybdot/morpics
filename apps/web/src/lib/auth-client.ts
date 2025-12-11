db0e634da99ccaedd3e0aa8fa58e1ea838103abb
Atyb. <180611249+atybdot@users.noreply.github.com>
2025-12-11 06:24:55 +0530

    dodpayment client

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -7,13 +7,15 @@ import {
 } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 import { env } from "@/env";
+import { dodopaymentsClient } from "@dodopayments/better-auth";
 
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_SERVER_URL,
+  baseURL: env.NEXT_PUBLIC_AUTH_URL,
   plugins: [
     inferAdditionalFields<typeof auth>(),
     organizationClient({}),
     apiKeyClient(),
     multiSessionClient(),
+    dodopaymentsClient(),
   ],
 });
