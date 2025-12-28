013bd165343130f3089dac378f9a91b89fa82ba0
Atyb. <180611249+atybdot@users.noreply.github.com>
2025-12-28 07:35:01 +0530

    fix:pricing page

--- apps/web/src/app/(home)/pricing/page.tsx
+++ apps/web/src/app/(home)/pricing/page.tsx
@@ -1,11 +1,23 @@
-import PricingGrid from "@/components/grids/pricing-grid";
+import { FaqsGridPrice } from "@/components/grids/faqs-grid";
+import PricingGrid from "@/components/grids/pricing";
 import React from "react";
 
 function Page() {
   return (
-    <div>
-      <PricingGrid />
-    </div>
+    <>
+      <section className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-0.5 bg-secondary gap-0.5 my-32">
+        <h2 className="col-span-full py-2 text-center text-xl bg-muted ">
+          Pricing
+        </h2>
+        <PricingGrid />
+      </section>
+      <section className="w-full grid grid-cols-1 p-0.5 bg-secondary gap-0.5 my-32">
+        <h2 className="col-span-full py-2 text-center text-xl bg-muted ">
+          Frequently Asked Questions
+        </h2>
+        <FaqsGridPrice />
+      </section>
+    </>
   );
 }
 
