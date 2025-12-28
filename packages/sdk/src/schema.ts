e15fa6ac6ffba68a52937a0747eb9ae2c0fc67e6
Atyb. <180611249+atybdot@users.noreply.github.com>
2025-12-28 07:43:30 +0530

    chore:refactor schema

--- packages/sdk/src/schema.ts
+++ packages/sdk/src/schema.ts
@@ -4,15 +4,9 @@ export const transformationQuerySchema = z.object({
   h: z.coerce.number().min(1, "height can not be smaller that 1px").optional(),
   w: z.coerce.number().min(1, "weight can not be smaller that 1px").optional(),
   r: z.coerce.number().optional(),
-  quality: z.coerce.number().min(10).max(100).optional().default(80),
-  format: z
-    .enum(["png", "webp", "avif", "jpeg", "tiff", "jp2"])
-    .optional()
-    .default("png"),
-  fit: z
-    .enum(["contain", "cover", "fill", "inside", "outside"])
-    .optional()
-    .default("cover"),
+  quality: z.coerce.number().min(10).max(100).optional(),
+  format: z.enum(["png", "webp", "avif", "jpeg", "tiff", "jp2"]).optional(),
+  fit: z.enum(["contain", "cover", "fill", "inside", "outside"]).optional(),
   position: z
     .enum([
       "top",
@@ -25,15 +19,14 @@ export const transformationQuerySchema = z.object({
       "left top",
       "center",
     ])
-    .optional()
-    .default("center"),
+    .optional(),
   blur: z.coerce
     .number()
     .min(0, "blur intensity cannot be less that 0")
     .optional(),
   grayscale: z.coerce.boolean().optional(),
 
-  keepMetadata: z.coerce.boolean().optional().default(false),
+  keepMetadata: z.coerce.boolean().optional(),
 });
 export type TransformationQuerySchema = z.infer<
   typeof transformationQuerySchema
