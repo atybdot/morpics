2733ddfe43d6b3401c137f4bcc13a79e9e648cd6
Atyb. <180611249+atybdot@users.noreply.github.com>
2025-12-28 07:35:08 +0530

    feat:add footer

--- apps/web/src/app/(home)/layout.tsx
+++ apps/web/src/app/(home)/layout.tsx
@@ -1,4 +1,4 @@
-import { ThemeSwitcherMultiButton } from "@/components/elements/theme-switcher-multi-button";
+import Footer from "@/components/grids/footer";
 import Header from "@/components/header";
 
 export default function RootLayout({
@@ -12,9 +12,7 @@ export default function RootLayout({
       <section className="mx-auto w-full h-full max-w-6xl bg-transparent px-4">
         {children}
       </section>
-      <footer className="mx-auto">
-        <ThemeSwitcherMultiButton />
-      </footer>
+      <Footer />
     </section>
   );
 }
--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -1,33 +1,36 @@
 import Link from "next/link";
-import FeaturesGrid from "@/components/grids/features-grid";
-import PricingGrid from "@/components/grids/pricing-grid";
+import FeaturesGrid from "@/components/grids/features";
+import PricingGrid from "@/components/grids/pricing";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import ImageMotion from "../../components/image-motion";
+import HowItWorksGrid from "@/components/grids/how-it-works";
+import { FaqsGridHome } from "@/components/grids/faqs-grid";
 export default function Home() {
   return (
     <section className="space-y-12">
       <section className="max-w-screen w-full relative sm:min-h-[calc(100svh-4rem)] md:content-center grid md:grid-cols-2 gap-8">
-        <div className="space-y-4 py-12">
-          <h1 className="text-6xl md:text-[5.5rem] font-semibold tracking-tighter text-balance">
-            The API your images deserve
+        <div className="space-y-4 py-12 content-center">
+          <h1 className="text-4xl md:text-[3.5rem] font-semibold tracking-tighter text-balance">
+            {/* The API your images deserve */}
+            URL-powered image manipulation
           </h1>
           <h2 className="text-sm text-balance md:text-lg mx-auto text-muted-foreground">
-            A clean, declarative API for transforming and optimizing images
+            Clean, declarative URLs that generate every image your app needs.
           </h2>
 
           <div className="mt-12 flex flex-col sm:flex-row gap-4 sm:gap-8">
             <Link
               href={"/dashboard"}
-              className={cn(buttonVariants({ size: "xl" }))}
+              className={cn(buttonVariants({ size: "lg" }))}
             >
               Try for free
             </Link>
             <Link
               href={"/playground"}
               className={cn(
-                buttonVariants({ size: "xl", variant: "secondary" }),
-                "bg-secondary/40 text-secondary-foreground/50",
+                buttonVariants({ size: "lg", variant: "secondary" }),
+                "bg-secondary text-secondary-foreground/50",
               )}
             >
               playground
@@ -38,14 +41,30 @@ export default function Home() {
           <ImageMotion />
         </div>
       </section>
-      <section className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-0.5 bg-secondary gap-0.5 my-16">
+      <section className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-0.5 bg-secondary gap-0.5 my-32 mt-0">
+        <h2 className="col-span-full py-2 text-center text-xl bg-muted ">
+          How It Works?
+        </h2>
+        <HowItWorksGrid />
+      </section>
+      <section className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-0.5 bg-secondary gap-0.5 my-32">
         <h2 className="col-span-full py-2 text-center text-xl bg-muted ">
           features
         </h2>
         <FeaturesGrid />
       </section>
-
-      <PricingGrid />
+      <section className="w-full grid grid-cols-1 p-0.5 bg-secondary gap-0.5 my-32">
+        <h2 className="col-span-full py-2 text-center text-xl bg-muted ">
+          Frequently Asked Questions
+        </h2>
+        <FaqsGridHome />
+      </section>
+      <section className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-0.5 bg-secondary gap-0.5 my-32">
+        <h2 className="col-span-full py-2 text-center text-xl bg-muted ">
+          Pricing
+        </h2>
+        <PricingGrid />
+      </section>
     </section>
   );
 }
